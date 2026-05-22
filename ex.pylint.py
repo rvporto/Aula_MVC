a89e7a232879e0aa7d9423263838e25bf36da06c
rvporto <victorporto98@hotmail.com>
2026-05-22 11:02:09 -0300

    Primeiro Commit

---
+++ ex.pylint.py
@@ -0,0 +1,9 @@
+print("Ola Mundo")
+
+def minha_funcao():
+    """Função que imprime uma mensagem."""
+    print("Estou na minha funcao")
+
+# snake_case -> variáveis e funções
+# camelCase -> Não consta
+# PascalCase -> nomes de classes
